aa881e2931393d1889ddf9f91415f0590e851d88
xiahe173 <qh22189009@gmail.com>
2026-05-27 16:23:30 +0800

    Add hardware version definitions

--- src/config.h
+++ src/config.h
@@ -1,5 +1,10 @@
 #pragma once
 
+// #define VFD_HW_VER_0_3
+#define VFD_HW_VER_0_2
+
+#ifdef VFD_HW_VER_0_2
+
 #define BUZZER_PIN 16
 
 #define VFD_RST_PIN 12
@@ -9,3 +14,19 @@
 #define F_EN_PIN P0
 
 #define PCF_ADDRESS 0x20
+
+#endif
+
+#ifdef VFD_HW_VER_0_3
+
+#define BUZZER_PIN 15
+
+#define VFD_RST_PIN 12
+#define VFD_CS_PIN 16
+
+#define HV_EN_PIN P3
+#define F_EN_PIN P2
+
+#define PCF_ADDRESS 0x20
+
+#endif
